16421cb8d040db17b860e3ed129d1f9a552d1d5f
RahulGattu912 <rahulgattu912@gmail.com>
2025-08-21 00:03:45 +0530

    added array args

--- command-line-args.sh
+++ command-line-args.sh
@@ -5,3 +5,6 @@ num2=$2
 sum=$(($num1+$num2))
 echo "The sum of $num1 and $num2 is $sum."
 echo "This script calculates the sum of two numbers passed as command-line arguments."
+
+arr=$* # $* captures all command-line arguments as a single string
+echo "The command-line arguments are: $arr"
