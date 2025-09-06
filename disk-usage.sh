ae07ef3c593756724ef42e44218ce483966b03f9
RahulGattu912 <rahulgattu912@gmail.com>
2025-09-06 18:26:55 +0530

    added disk usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -8,4 +8,8 @@ do
     USAGE=$(echo $line | awk -F " " '{print $6}' | cut -d'%' -f1) # get usage percentage without %
     PARTITION=$(echo $line | awk -F " " '{print $N}')
     echo "Partition: $PARTITION, Usage: $USAGE"
+    if [ $USAGE -gt $DISK_THRESHOLD ] 
+    then
+        echo "Warning: Disk usage for partition $PARTITION is above threshold ($USAGE% > $DISK_THRESHOLD%)"
+    fi
 done <<< $DISK_USAGE
