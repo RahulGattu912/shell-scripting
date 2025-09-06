a7ce4d1ee6bc79316ec71d5582ebe246867a32c6
RahulGattu912 <rahulgattu912@gmail.com>
2025-09-06 18:19:10 +0530

    added disk usage

---
+++ disk-usage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5 # real projects will monitor for 70
+
+while read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d'%' -f1) # get usage percentage without %
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    echo "Partition: $PARTITION, Usage: $USAGE"
+done <<< $DISK_USAGE
