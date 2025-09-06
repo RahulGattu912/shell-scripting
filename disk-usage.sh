73324a1aa8bd223c3d0920e53a981542620569c1
RahulGattu912 <rahulgattu912@gmail.com>
2025-09-06 18:22:37 +0530

    added disk usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -5,7 +5,7 @@ DISK_THRESHOLD=5 # real projects will monitor for 70
 
 while read -r line
 do
-    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d'%' -f1) # get usage percentage without %
-    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    USAGE=$(echo $line | awk -F " " '{print $6}' | cut -d'%' -f1) # get usage percentage without %
+    PARTITION=$(echo $line | awk -F " " '{print $N}')
     echo "Partition: $PARTITION, Usage: $USAGE"
 done <<< "$DISK_USAGE"
